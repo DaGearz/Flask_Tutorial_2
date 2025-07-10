27b466ca1d50f68910d2ae781d1e614a3bd8da61
DaGearz <twill003@gmail.com>
2025-07-10 11:01:35 -0700

    done with this project

--- market/forms.py
+++ market/forms.py
@@ -1,37 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
 
-
 class RegisterForm(FlaskForm):
-
     def validate_username(self, username_to_check):
         user = User.query.filter_by(username=username_to_check.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
-    def validate_email(self, email_to_check):
-        email = User.query.filter_by(email=email_to_check.data).first()
-        if email:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError('Username already exists! Please try a different username')
 
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
 
-    username = StringField(label='User Name:', validators=[DataRequired(), Length(min=2, max=30)])
-    email = StringField(label='Email Address:', validators=[DataRequired(), Length(min=2, max=50), Email()])
-    password1 = PasswordField(label='Password:', validators=[DataRequired(), Length(min=6)])
-    password2 = PasswordField(label='Confirm Password:', validators=[DataRequired(), EqualTo('password1')])
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
-    submit = SubmitField(label='Login')
+    submit = SubmitField(label='Sign in')
 
 class PurchaseItemForm(FlaskForm):
     submit = SubmitField(label='Purchase Item!')
 
 class SellItemForm(FlaskForm):
-    submit = SubmitField(label='Purchase Item!')
-
-
+    submit = SubmitField(label='Sell Item!')
--- market/models.py
+++ market/models.py
@@ -7,16 +7,19 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(30), unique=True, nullable=False)
-    email=db.Column(db.String(50), unique=True, nullable=False)
-    password_hash=db.Column(db.String(60), nullable=False)
-    budget=db.Column(db.Float(), nullable=False, default=1000)
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
 
     @property
     def prettier_budget(self):
-        return f"${self.budget:,.2f}"
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f"{self.budget}$"
 
     @property
     def password(self):
@@ -29,17 +32,28 @@ class User(db.Model, UserMixin):
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
 
-    def __repr__(self):
-        return f'User {self.username}'
+    def can_sell(self, item_obj):
+        return item_obj in self.items
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
-    barcode = db.Column(db.String(12), nullable=False, unique=True)
-    price = db.Column(db.Float(), nullable=False)
-    description = db.Column(db.String(1024), nullable=False, unique=True)
-    owner = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=True)
-
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
+
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
+
+    def sell(self, user):
+        self.owner = None
+        user.budget += self.price
+        db.session.commit()
